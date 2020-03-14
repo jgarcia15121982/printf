25e8d9e3efecdddfc87f540355825dd4c2744f09
AlejoCasti <acastiblanco8@gmail.com>
2020-03-14 22:35:53 +0000

    [ADD] char function

--- print_functions.c
+++ print_functions.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * p_char - save character to buffer
+ * @list: list of args
+ * @p_buf: pointer to buffer
  */
-void p_char()
+void p_char(va_list list, char *p_buf)
 {
-	
+	*p_buf = va_arg(list, int);
+	p_buf++;
 }
-
+/**
+ * diccio - save character to buffer
+ * Return: pointer to new dictionary
+ */
 print *diccio()
 {
 	print *dic = malloc(sizeof(print) * 1);
