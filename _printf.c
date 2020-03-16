45e4dc0c6904353d7d754a8fc2ba6688d2f93d45
AlejoCasti <acastiblanco8@gmail.com>
2020-03-16 20:04:19 +0000

    [FIX] unknow flag

--- _printf.c
+++ _printf.c
@@ -13,6 +13,26 @@ int size_buf(char *point_buf)
 	return (i);
 }
 /**
+ * val_unknown - validate if the flag is unknow
+ * @j: counter to dic
+ * @i: counter to format
+ * @dic: pointer to dictionary
+ * @p_buf: pointer to last position of buffer
+ * @format: string
+ * Return: last position of buffer
+ */
+char *val_unknown(int j, int i, print *dic, char *p_buf, const char *format)
+{
+	if (dic[j].flag == '\0')
+	{
+		*p_buf = *(format - 1 + i);
+		p_buf++;
+		*p_buf = *(format + i);
+		p_buf++;
+	}
+	return (p_buf);
+}
+/**
  * _printf - print all arguments that put int
  * @format: string and format to print
  * Return: Number of characters printed
@@ -27,11 +47,7 @@ int _printf(const char *format, ...)
 
 	va_start(list, format);
 	if (!buffer || !dic || !format)
-	{
-		free(buffer);
-		free(dic);
-		return (-1);
-	}
+		return (free(buffer), free(dic), -1);
 	if (format[0] == '%' && format[1] == '\0')
 		return (-1);
 	while (format && format[i])
@@ -42,15 +58,19 @@ int _printf(const char *format, ...)
 			p_buf++;
 		}
 		else
-		{			
+		{
 			i++;
 			j = 0;
 			while (dic[j].flag && format[i] != '\0')
 			{
 				if (format[i] == dic[j].flag)
+				{
 					p_buf = dic[j].p_fun(list, p_buf);
+					break;
+				}
 				j++;
 			}
+			p_buf = val_unknown(j, i, dic, p_buf, format);
 		}
 		i++;
 	}
