1e8c5bfca4850c17da5738c3a3a2df53211a9cb4
jgarcia15121982 <johfregarcia@gmail.com>
2020-03-18 03:32:59 +0000

    [UPDATE] Libraries

--- holberton.h
+++ holberton.h
@@ -1,9 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 /**
  * struct s_print - flag struct
  * @flag: flag
